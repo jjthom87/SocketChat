2aba77dbb464ee305b1567e56aaa75d7bc305d8f
Jared Thomas <jjthom87@yahoo.com>
2016-09-14 18:17:23 -0400

    global connection successful

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http)
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io');
+
+	socket.on('message', function(message){
+		console.log('Message received: ' + message.text);
+
+		// io.emit sends to every person
+		//below sends to every person but the sender
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to the chat'
+	});
 });
 
 http.listen(PORT, function (){
