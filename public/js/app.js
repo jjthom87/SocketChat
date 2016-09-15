55a1515e95f95ef974691f0cc65feae8f4051eb5
Jared Thomas <jjthom87@yahoo.com>
2016-09-14 20:51:15 -0400

    can only see messages within that room

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,34 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room') || 'Anonymous';
 var socket = io();
-var time = moment().format("MMM Do YY");
-
-socket.on('connect', function(){
-	console.log('Connected to socket.io server!')
-});
-
-socket.on('message', function(message){
-	console.log('New message:');
-	console.log(message.text);
-	var timeStamp = moment.utc(message.timestamp);
-
-	$('.messages').append('<p><strong>' + timeStamp.local().format('h:mm a') + ": " + '</strong>' + message.text + '</p>');
-});
-
-//handles submitting of message
 var form = $('#message-form');
 var newMessage = $('#inputMessage');
 //var newMessage = form.find('input[name=message]');
+
+socket.on('connect', function(){
+	console.log('Connected to socket.io server!');
+
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
+});
+
+socket.on('message', function(message){
+	var timeStamp = moment.utc(message.timestamp);
+	var messages = $('.messages');
+
+	$('#roomTag').text("Welcome to the " + room + " room")
+	messages.append('<p><strong>' + message.name + ' ' + timeStamp.local().format('h:mm a') + '</strong></p>');
+	messages.append('<p>' + message.text + '</p>');
+});
 
 form.on('submit', function(e){
 	e.preventDefault();
 
 	socket.emit('message', {
+		room: room,
+		name: name,
 		text: newMessage.val()
 	});
 
--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 var PORT = process.env.PORT || 5000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
+
+var clientInfo = {};
 
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io');
 
+	socket.on('joinRoom', function(req){
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message',{
+			name: 'System',
+			text: req.name + ' has joined',
+			timestamp: moment().valueOf()
+		});
+	});
+
 	socket.on('message', function(message){
 		console.log('Message received: ' + message.text);
 
-		io.emit('message', message);
+		message.timestamp = moment().valueOf();
+		io.to(clientInfo[socket.id].room).emit('message', message);
 		//below sends to every person but the sender
 		// socket.broadcast.emit('message', message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to the chat'
+		name: 'Hello',
+		text: 'Welcome to the chat',
+		timestamp: moment().valueOf()
 	});
 });
 
